56475de30b7d1202ae0730c03cb0fbbf4a9486f8
Caleb Knight <knightca@oregonstate.edu>
2022-03-17 19:35:47 -0700

    Frustrated that I can't get the first two tests to pass even though I know i've done it a million times

--- spec/tile_set_spec.rb
+++ spec/tile_set_spec.rb
@@ -3,13 +3,14 @@ require_relative '../lib/tile_set'
 describe 'A tile set' do
 
   describe '::generate' do
-
     it 'returns an array of 9 tiles by default' do
-      raise "Start here"
+      quantity = TileSet.generate
+      expect(quantity).to eq("9")
     end
 
     it 'returns an array of tiles of a specified length' do
-      skip
+      quantity = TileSet.generate(8)
+      expect(quantity).to eq(8)
     end
 
     it 'returns an array of tiles whose values increase from 1, by 1' do
