98c3bd2e2e7e2afee4e95d9e8759d2ce802eb67a
Caleb Knight <knightca@oregonstate.edu>
2022-03-17 19:57:14 -0700

    Finishing up

--- spec/tile_set_spec.rb
+++ spec/tile_set_spec.rb
@@ -5,7 +5,7 @@ describe 'A tile set' do
   describe '::generate' do
     it 'returns an array of 9 tiles by default' do
       quantity = TileSet.generate
-      expect(quantity).to eq("9")
+      expect(quantity).to eq(9)
     end
 
     it 'returns an array of tiles of a specified length' do
